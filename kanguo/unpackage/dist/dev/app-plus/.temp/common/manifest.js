d6c1b38db06c2ee296ce979bac8d459f16acba63
iszhangzhijie <714816371@qq.com>
2018-11-24 18:09:20 +0800

    Modify

--- kanguo/unpackage/dist/dev/app-plus/.temp/common/manifest.js
+++ kanguo/unpackage/dist/dev/app-plus/.temp/common/manifest.js
@@ -34,7 +34,7 @@
 /******/
 /******/ 	// objects to store loaded and loading chunks
 /******/ 	var installedChunks = {
-/******/ 		23: 0
+/******/ 		25: 0
 /******/ 	};
 /******/
 /******/ 	// The require function
--- kanguo/unpackage/dist/dev/app-plus/kanguo/pages/kanju/kanju.js
+++ kanguo/unpackage/dist/dev/app-plus/kanguo/pages/kanju/kanju.js
@@ -1,7 +1,7 @@
 
       !(function(){
         var uniAppViewReadyCallback = function(){
-          setCssToHead([[2,1],],undefined,{path:"./pages/kanju/kanju.wxss"})();
+          setCssToHead([[2,1],"body { display: -webkit-box; display: -webkit-flex; display: flex; }\n.",[1],"index { display: -webkit-box; display: -webkit-flex; display: flex; -webkit-box-flex: 1; -webkit-flex: 1; flex: 1; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; flex-direction: column; overflow: hidden; height: 100%; }\n.",[1],"list { width: ",[0,750],"; height: 100%; }\n.",[1],"swiper-tab { width: 100%; white-space: nowrap; line-height: ",[0,100],"; height: ",[0,100],"; border-bottom: 1px solid #c8c7cc; }\n.",[1],"swiper-tab-list { font-size: ",[0,30],"; color: #555; }\n.",[1],"active { color: #007AFF; }\n.",[1],"swiper-box { -webkit-box-flex: 1; -webkit-flex: 1; flex: 1; width: 100%; height: calc(100% - ",[0,100],"); }\n.",[1],"loadmore { height: ",[0,70],"; width: ",[0,750],"; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; flex-direction: column; -webkit-box-pack: center; -webkit-justify-content: center; justify-content: center; }\n.",[1],"loadmore-text { font-size: ",[0,30],"; text-align: center; color: #999999; }\n",],undefined,{path:"./pages/kanju/kanju.wxss"})();
 document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/kanju/kanju.wxml') } }));
         }
         if(window.__uniAppViewReady__){
